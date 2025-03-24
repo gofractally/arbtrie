7eaa5e820b8c447ee1f3077d760ecfeeb6441916
Daniel Larimer <dlarimer@gmail.com>
2025-03-24 02:03:24 -0500

    Deleting deprecated file

--- src/block_allocator.cpp
+++
@@ -1,5 +0,0 @@
-#include <arbtrie/block_allocator.hpp>
-
-// This file is now a stub since arbtrie::block_allocator
-// is now just an alias for sal::block_allocator.
-// All implementation is in the sal library.
